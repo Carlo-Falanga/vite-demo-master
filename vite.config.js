9783ac116a43f9fced60eb5de9210ca9ecd17d1c
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-27 17:03:08 +0200

    update base path in Vite configuration to '/vite-demo-master/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-demo/',
+  base: '/vite-demo-master/',
   build: {
     outDir: 'dist',
     sourcemap: false,
